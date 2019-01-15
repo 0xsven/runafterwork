e2f1b17f81894c774faf36d04e4fee3ab5e5c05c
Sven Schmidt <0xsven@gmail.com>
2019-01-15 17:38:56 +0100

    Route #2

--- src/data/dates.js
+++ src/data/dates.js
@@ -1,7 +1,14 @@
 const dates = [
   {
     highlight: true,
-    date: '2019-01-09',
+    date: '2019-01-16T19:00:00.000Z',
+    route: 'Kreuzberg Kanal Run 2',
+    distance: 4200,
+    komootId: 54727955,
+  },
+  {
+    highlight: false,
+    date: '2019-01-09T19:00:00.000Z',
     route: 'Kreuzberg Kanal Run 1',
     distance: 3400,
     komootId: 54117757,
